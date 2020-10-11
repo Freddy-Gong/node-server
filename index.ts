1aabb15da93532ea9f0bf645f6be9dc515475180
Freedy <21914103@zju.edu.cn>
2020-10-11 10:50:23 +0800

    基本完成静态服务器

--- index.ts
+++ index.ts
@@ -1,31 +1,39 @@
 import * as http from 'http'
 import { ServerResponse, IncomingMessage } from 'http'
+import * as fs from 'fs'
+import * as p from 'path'
+import * as url from 'url'
 
 const server = http.createServer()
-
+const publicDir = p.resolve(__dirname, 'public')
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    console.log('aaaa')
-    console.log(request.method)
-    console.log(request.httpVersion)
-    console.log(request.url)
-    console.log(request.headers)
-    const array = []
-    request.on('data', (chunk) => {
-        array.push(chunk)
+    const { method, url: pathName, headers } = request
+    const { pathname, search } = url.parse(pathName)
+    let cacheAge = 3600 * 24 * 365
+    if (method === 'POST') {
+        response.statusCode = 405
+        response.end()
+        return
+    }
+    let filename = pathname.substr(1)
+    if (filename === '') {
+        filename = 'index.html'
+    }
+    fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            if (error.errno === -4058) {
+                response.statusCode = 404
+                response.end('文件不存在')
+            } else {
+                response.statusCode = 500
+                response.end('服务器繁忙')
+            }
+        } else {
+            response.setHeader('Catch-Control', `public,max-age=${cacheAge}`)
+            response.end(data.toString())
+        }
     })
-    request.on('end', () => {
-        const body = Buffer.concat(array).toString()
-        console.log(body)
 
-
-        response.statusCode = 404
-        response.setHeader('x-freddy', 'aaaa')
-        response.setHeader('Content-Type', 'img/png')
-
-        response.write('1\n')
-        response.end('12345')
-
-    })
 })
 
 server.listen(8888)
